639d76b641e5c19a765bfc04860996f0e7fe1a9a
VicTheM <117105067+VicTheM@users.noreply.github.com>
2024-03-22 17:53:28 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Install flask from pip.
 
 exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
+  command =>  'sudo pip3 install flask==2.1.0',
   path    =>  '/usr/bin/',
   unless  =>  'pip3 list | grep flask',
 }
