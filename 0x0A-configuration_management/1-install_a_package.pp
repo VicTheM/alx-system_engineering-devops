1f0fbc5adc2d74d685bfa4e7a34ecbea28199bac
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-03-22 17:38:22 +0100

    fIXED ERRORS

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,5 @@
 
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # This script kills a process named killmenow
 
 exec { 'command':
-  command => '/bin/pkill "killmenow"' # Path to the executable command
+  command => '/bin/pkill "killmenow"'
 }
