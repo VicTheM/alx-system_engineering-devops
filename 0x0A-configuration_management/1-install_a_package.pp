a51aa77e5fc06acedf8a9aea5e03e020c1b8fd64
Victory Chibuike <victorychibuike121@gmail.com>
2024-04-19 18:34:16 +0100

    dONE

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,8 @@
-# installs Flask 2.1.0
+# Installs Flask, version 2.1.0 from pip3
 
-class flask {
-  # Ensure python3-pip is installed
-  package {'python3-pip':
-    provider => 'apt',
-  }
-
-  # Install Flask 2.1.0 via pip3
-  exec {'install-flask':
-    path     => '/usr/bin:$PATH',
-    command  => 'pip3 install Flask==2.1.0',
-    require  => Package['python3-pip'],
-    notify   => Service['httpd'],
-    logoutput => true,
-  }
+package { 'flask':
+	name => 'flask',
+	provider => pip3,
+	ensure => '2.1.0',
 }
+
