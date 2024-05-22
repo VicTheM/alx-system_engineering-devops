51e3d95e7758f5f3cf6f712e5b732fb2a26ab855
Victory Chibuike <victorychibuike121@gmail.com>
2024-05-22 22:31:35 +0100

    nil

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This script retrievs a user task details from a REST API"""
+
+import sys
+import requests
+
+userID = int(sys.argv[1])
+nameURL = "https://jsonplaceholder.typicode.com/users" + "/" + str(userID)
+todoURL = "https://jsonplaceholder.typicode.com/todos"
+
+res = requests.get(nameURL)
+user = res.json()
+
+params = {
+        "userId": userID
+        }
+res = requests.get(todoURL, params=params)
+tasks = res.json()
+
+completed = 0
+total = len(tasks)
+for task in tasks:
+        if (task["completed"] is True):
+            completed = completed + 1
+
+
+print("Employee {} is done with tasks({}/{}):".format(user["name"], completed, total))
+for task in tasks:
+    if task["completed"] is True:
+            print("\t {}".format(task["title"]))
