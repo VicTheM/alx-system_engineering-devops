0672c21da4eb9a2e99618dac8301dbdfb1d0659c
Victory Chibuike <victorychibuike121@gmail.com>
2024-08-16 05:18:37 +0100

    retabbed file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,23 +6,23 @@ import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of
-    the first 10 hot posts listed for a given subreddit"""
-    
-    url = "https://www.reddit.com/r/{}/hot.json?".format(subreddit)
-    headers = {"User-Agent": "MyBot/1.0 (by Victory10X)"}
-    params = {"limit": 10}
+	"""Queries the Reddit API and prints the titles of
+	the first 10 hot posts listed for a given subreddit"""
 
-    response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        if posts and isinstance(posts, list):
-            for post in posts[:10]:
-                print(post['data']['title'])
-    else:
-        print(None)
+	url = "https://www.reddit.com/r/{}/hot.json?".format(subreddit)
+	headers = {"User-Agent": "MyBot/1.0 (by Victory10X)"}
+	params = {"limit": 10}
+
+	response = requests.get(
+			url,
+			headers=headers,
+			params=params,
+			allow_redirects=False)
+	if response.status_code == 200:
+		data = response.json()
+		posts = data['data']['children']
+		if posts and isinstance(posts, list):
+			for post in posts[:10]:
+				print(post['data']['title'])
+	else:
+		print(None)
---
+++ 0x16-api_advanced/1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function sha"""
+
+import requests
+
+def top_ten(subreddit):
+    """QUERIES THE REDDIT API"""
+
+    url = "https"
