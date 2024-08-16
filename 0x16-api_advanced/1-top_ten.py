d7b3f358cef54918208c3b3d648a7817e61012d9
Victory <victorychibuike121@gmail.com>
2024-08-16 05:26:16 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """Defines a top_ten function."""
-
-
 import requests
 
 
 def top_ten(subreddit):
     """Queries the Reddit API and prints the titles of
     the first 10 hot posts listed for a given subreddit"""
-
     url = "https://www.reddit.com/r/{}/hot.json?".format(subreddit)
     headers = {"User-Agent": "MyBot/1.0 (by Victory10X)"}
     params = {"limit": 10}
