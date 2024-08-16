e8702bf2b7e22fff2a5420308b493de67efb5c13
Victory <victorychibuike121@gmail.com>
2024-08-16 04:43:21 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,25 @@
-"""This script queries the Reddit API and prints the titles of the first 10
-hot posts listed for a given subreddit."""
-
-
+#!/usr/bin/python3
+"""Defines a top_ten function."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    If an invalid subreddit is given, prints None.
-    """
+    """Queries the Reddit API and prints the titles of
+    the first 10 hot posts listed for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0 (by Cipher10X)"}
+    params = {"limit": 10}
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "VictoryBot"}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
+        if posts and isinstance(posts, list):
+            for post in posts[:10]:
+                print(post['data']['title'])
     else:
         print(None)
