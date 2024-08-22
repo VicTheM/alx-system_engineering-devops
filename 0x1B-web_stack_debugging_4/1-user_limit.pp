429263a07d64d49becbdf87159e123b9007c2800
Victory <victorychibuike121@gmail.com>
2024-08-22 11:52:55 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # This manifest modifies the limit descriptor file for a user
 
 exec { 'change the amount of open files for the holberton user':
-        command => 'sed -i \'s/^holberton hard.*$/holberton hard nofile 4096/g\' /etc/security/limits.conf',
+        command => 'sed -i \'s/^holberton hard.*$/holberton hard nofile 50000/g\' /etc/security/limits.conf',
         path => '/bin/',
 }
 
 
 exec { 'for soft links':
-        command => 'sed -i \'s/^holberton soft.*$/holberton soft nofile 4096/g\' /etc/security/limits.conf',
+        command => 'sed -i \'s/^holberton soft.*$/holberton soft nofile 40000/g\' /etc/security/limits.conf',
         path => '/bin/',
 }
